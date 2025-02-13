3fd5d3f9fc68594939a2dc37ac61736172b74dbc
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-02-12 21:21:47 -0600

    Update .gitignore

---
+++ src/main/java/guru/springframework/fizzery/FizzeryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.fizzery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzeryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/fizzery/FizzeryApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.fizzery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
